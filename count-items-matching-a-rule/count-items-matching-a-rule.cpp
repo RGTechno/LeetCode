3814b8396e5d2b858211cacbda742505204f12c1
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-12-26 16:07:17 +0530

    Time: 72 ms (64.64%), Space: 31 MB (41.28%) - LeetHub

---
+++ count-items-matching-a-rule/count-items-matching-a-rule.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        
+        int n=items.size();
+        
+        int count=0;
+        
+        for(int i=0;i<n;i++){
+            if(ruleKey=="type"){
+                if(items[i][0]==ruleValue) count++;
+            }
+            else if(ruleKey=="color"){
+                if(items[i][1]==ruleValue) count++;
+            }
+            else if(ruleKey=="name"){
+                if(items[i][2]==ruleValue) count++;
+            }
+        }
+        
+        return count;
+        
+    }
+};
