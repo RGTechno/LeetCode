2d3d999bb3226e32caccea0e7a13c2cedc712854
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2022-01-18 17:36:07 +0530

    Time: 4 ms (99.91%), Space: 20.3 MB (69.55%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        
+        // 0 can be changed with 1
+        
+        // on N consective zeroes max N/2 1's can be placed
+        
+        int max=0;
+        
+        for(int i=0;i<flowerbed.size();i++){
+            int m=i;
+            int czero=0;
+            if(flowerbed[i]==0){
+                while(m<flowerbed.size() and flowerbed[m]==0){
+                    czero++;
+                    m++;
+                }
+                
+                if(i==0 and m==flowerbed.size()){
+                    max+=((czero+1)/2);
+                }
+                
+                else if(i==0 or m==flowerbed.size()){
+                    max+=(czero/2);
+                }              
+                
+                else if(czero>2){
+                    if(czero&1){
+                        max+=(czero/2);
+                    }
+                    else {
+                        max+=((czero-1)/2);
+                    }
+                }
+                i=m;
+            }
+        }
+        
+        if(n==1 and flowerbed.size()==1 and flowerbed[0]!=1) return true;        
+        if(max>=n) return true;
+        return false;
+        
+    }
+};
