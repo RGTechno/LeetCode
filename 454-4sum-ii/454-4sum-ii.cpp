c897145917a6a7f9dc4280b26630414f3c6d887e
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2022-02-03 14:12:18 +0530

    Time: 636 ms (6.59%), Space: 24.7 MB (26.52%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public: 
+
+    // O(N^2)
+    
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {        
+        
+        map<int,int> mp;
+        
+        for(auto i:nums1){
+            for(auto j:nums2){
+                mp[(i+j)]++;
+            }
+        }
+        
+        int count = 0;
+        
+        for(auto i:nums3){
+            for(auto j:nums4){
+                if(mp.count(-(i+j))){
+                    count+=mp[-(i+j)];
+                }
+            }
+        }        
+        
+        return count;
+    }
+    
+/*
+
+    O(N^3)
+    
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {        
+        map<int,int> mp;
+        
+        for(auto i:nums4){
+            mp[i]++;
+        }
+        
+        int n=nums1.size();
+        
+        int count = 0;        
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++){
+                    long long int sum = nums1[i]+nums2[j]+nums3[k];
+                    
+                    long long int find = (-1)*sum;
+                    
+                    // OR BINARY SEARCH ON NUMS4
+                    
+                    if(mp.count(find)){
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;       
+        
+    }
+*/
+};
