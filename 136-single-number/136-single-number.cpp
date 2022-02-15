ead978ccc83fc3b9b2be1e54176c798904eb1796
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2022-02-15 11:53:34 +0530

    Time: 16 ms (91.50%), Space: 16.9 MB (85.89%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> mp;
         
-        for(auto i:nums) mp[i]++;
+        int ans = 0;
         
-        int ans;
-        
-        for(auto i=mp.begin();i!=mp.end();i++){
-            if(i->second != 2) {
-                ans = i->first;
-                break;
-            }
-        }
+        for(auto &i:nums) ans^=i;
         
         return ans;
+        
     }
 };
