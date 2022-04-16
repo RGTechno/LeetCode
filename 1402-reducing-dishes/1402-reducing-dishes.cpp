7dce4456c2c257fb9801e8d1cab3641f89c07118
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2022-04-16 17:11:26 +0530

    Time: 35 ms (22.06%), Space: 9.2 MB (21.16%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    int dp[501][501];
+
+    int solve(vector<int> &v,int time=1,int idx=0){
+        if(idx==v.size()){
+            return 0;
+        }
+        if(dp[idx][time]!=-1) return dp[idx][time];
+        int ans1=(v[idx]*time)+solve(v,time+1,idx+1);
+        int ans2=solve(v,time,idx+1);
+        
+        return dp[idx][time] = max(ans1,ans2);
+    }
+public:
+    Solution(){
+        memset(dp,-1,sizeof(dp));
+    }
+    
+    int maxSatisfaction(vector<int>& satisfaction) {
+        
+        int n = satisfaction.size();
+        
+        sort(satisfaction.begin(),satisfaction.end());
+        
+        int ans = 0;
+        
+        return solve(satisfaction);
+        
+        return ans;
+        
+    }
+};
