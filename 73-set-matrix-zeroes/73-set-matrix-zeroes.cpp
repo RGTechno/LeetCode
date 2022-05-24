3d3605ef852225e85ed043f6240a1f7f5b434cc1
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2022-05-24 17:34:57 +0530

    Time: 22 ms (37.18%), Space: 13.2 MB (54.34%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,40 +1,34 @@
 class Solution {
-public:
     
-    void makeZero(vector<vector<bool>>& visited,vector<vector<int>>& arr,int r,int c,int m,int n){
-        
-        for(int i=0;i<n;i++){
-            if(arr[r][i]!=0){
-                visited[r][i]=true;
+    void transform(vector<vector<int>> &v,int m,int n,int r,int c){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==r or j==c) v[i][j]=0;
             }
-            arr[r][i]=0;
-            
-        }
-        
-        for(int i=0;i<m;i++){  
-            
-            if(arr[i][c]!=0){
-                visited[i][c]=true;
-            }
-            arr[i][c]=0;
-            
         }
     }
     
+public:
     void setZeroes(vector<vector<int>>& matrix) {
+        
+        vector<pair<int,int>> v;
         
         int m = matrix.size();
         int n = matrix[0].size();
         
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
-        
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(matrix[i][j]==0 and !visited[i][j]){
-                    makeZero(visited,matrix,i,j,m,n);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    v.push_back({i,j});
                 }
             }
         }
         
+        for(auto &p:v){
+            int i = p.first;
+            int j = p.second;
+            
+            transform(matrix,m,n,i,j);
+        }
     }
 };
