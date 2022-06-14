e7d5742c101872480fc2bddd43ca8065670da093
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2022-06-14 15:33:15 +0530

    Time: 6 ms (55.60%), Space: 9.6 MB (52.32%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -2,11 +2,24 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        // brute force
-        for(auto &i:matrix){
-            for(auto &j:i){
-                if(j==target) return true;
-            }
+        // brute force (n*n)
+//         for(auto &i:matrix){
+//             for(auto &j:i){
+//                 if(j==target) return true;
+//             }
+//         }
+        
+//         return false;
+        
+        //  optimised ()
+        int m=matrix.size();
+        int n=matrix[0].size();
+        
+        int j=n-1,i=0;
+        while(j>=0 and i<m){
+            if(matrix[i][j]==target) return true;
+            if(matrix[i][j]>target) j--;
+            else i++;
         }
         
         return false;
