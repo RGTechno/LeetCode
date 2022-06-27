0ed59c9c9233fa289b04cf608e4d066f27def38a
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2022-06-27 11:08:20 +0530

    Time: 94 ms (9.10%), Space: 13.7 MB (17.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int minPartitions(string s) {
-        
         int n = s.size(); // n-digit deci-binary's  req
         
         sort(s.begin(),s.end());
         
         return s[n-1]-'0';
-        
     }
 };
